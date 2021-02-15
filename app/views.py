6ef16311c511fb67d39b9e39ad694298b1230c8c
Donald Berry <dberry.offical@gmail.com>
2021-02-14 20:21:03 -0500

    Date Form

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
@@ -24,6 +25,14 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile ():
+    "Renders the website's profile page."
+    print(datetime.datetime.now())
+    return render_template('profile.html')
+
+def format_date_joined(year, month, day):
+    return datetime.date(year, month, day)
 
 ###
 # The functions below should be applicable to all Flask apps.
